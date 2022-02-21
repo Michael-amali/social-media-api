e35ef9a250f2eaf28868f92b29e9909cdd3a9cbb
Michael-amali <michael.acheampong@amalitech.com>
2022-02-21 16:05:45 +0100

    Updated the conversation creation to not create another conversation if one already exist and conversation deletion

--- routes/conversations.js
+++ routes/conversations.js
@@ -1,20 +1,32 @@
 const router = require("express").Router();
 const Conversation = require("../models/Conversation");
+const authenticate = require("../middlewares/auth.middleware");
 
 // create conversation
 router.post('/', async (req, res)=>{
 
-    const newConversation = new Conversation({
-        members: [req.body.senderId, req.body.receiverId]
-    });
+    const conversation = await Conversation.findOne({
+        members: {$all: [req.body.senderId, req.body.receiverId]}
+    })
+    // There's no existing conversation so create new one
+    if(!conversation){
+        const newConversation = new Conversation({
+            members: [req.body.senderId, req.body.receiverId]
+        });
 
-    try{
-        const savedConversation = await newConversation.save();
-        return res.status(200).json(savedConversation);
+        try{
+            const savedConversation = await newConversation.save();
+            return res.status(200).json(savedConversation);
+        }
+        catch(err){
+            return res.status(500).json(err);
+        } 
     }
-    catch(err){
-        return res.status(500).json(err);
-    } 
+    // It means the sender and receiver have already created conversation, so return the old conversation
+    else {
+        return res.status(200).json(conversation);
+    }
+
 });
 
 
@@ -34,7 +46,6 @@ router.get('/:userId', async(req, res)=>{
 
 
 // get conv includes two userId
-
 router.get('/find/:firstUserId/:secondUserId', async(req, res)=>{
     try{
         // it will go through each conversation and check members array to see if firstUserId and secondUserId is in the array, if so, it will return the conversations
@@ -48,6 +59,32 @@ router.get('/find/:firstUserId/:secondUserId', async(req, res)=>{
     } 
 })
 
+
+// delete conversation - my-inclusion
+router.delete('/:userId/:partnerId/:conversationId', authenticate.verifyToken, async(req, res)=>{
+
+    try{
+       const conversation = await Conversation.findOne({
+            members: {$all: [req.params.userId, req.params.partnerId]}
+        })
+
+        if(conversation._id.toString() === req.params.conversationId){
+            try {
+                await conversation.deleteOne()
+                return res.status(200).json('Conversation successfully deleted');
+            }
+            catch(err){
+                return res.status(500).json(err);
+            } 
+        }
+        else{
+            return res.status(403).json("You can only delete your conversation");
+        }
+    }
+    catch(err){
+        return res.status(500).json(err);
+    } 
+})
 
 
 module.exports = router;
